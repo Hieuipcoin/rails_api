3746dddf22f8ab86aa9ef61496c9d252000efb11
Riki <hieult@ipcoin.vn>
2018-10-23 23:06:02 +0700

    remaining api

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -1,5 +1,6 @@
 module V1
   class ArticlesController < ApplicationController
+
     def index
       all_articles = Article.order('created_at DESC')
       render json: {
@@ -8,5 +9,67 @@ module V1
           data: all_articles
       }, status: :ok
     end
+
+    def show
+      specific_article = Article.find(params[:id])
+      render json: {
+          status: 'SUCCESS',
+          message: "Loaded an article with id = #{params[:id]}",
+          data: specific_article
+      }, status: :ok
+    end
+
+    def create
+      new_article = Article.new(article_params)
+      if new_article.save
+        render json: {
+            status: 'SUCCESS',
+            message: "Saved the article }",
+            data: new_article
+        }, status: :ok
+      else
+        render json: {
+            status: 'eRROR',
+            message: "can not Saved the article }",
+            data: new_article.errors
+        }, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      destroy_article = Article.find(params[:id])
+      destroy_article.destroy
+      render json: {
+          status: 'SUCCESS',
+          message: "delete the article }",
+          data: destroy_article
+      }, status: :ok
+    end
+
+
+    def update
+      update_article = Article.find(params[:id])
+      if update_article.update_attributes(article_params)
+        render json: {
+            status: 'SUCCESS',
+            message: "updated the article }",
+            data: update_article
+        }, status: :ok
+      else
+        render json: {
+            status: 'ERROR',
+            message: "can not update the article }",
+            data: update_article.errors
+        }, status: :unprocessable_entity
+      end
+    end
+
+
+
+    private
+    def article_params
+      params.permit(:title, :body)
+    end
+
   end
 end
